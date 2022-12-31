a565ff0b127e82817e075172f536bc4969df646f
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-31 19:51:40 +0000

    wanted the python file to reflect nb

--- boston-housing-data-21-22/bmp-data.py
+++ boston-housing-data-21-22/bmp-data.py
@@ -1,9 +1,20 @@
 #Import relevant libs
+
 import pandas as pd
 from pandas import read_csv
 import numpy as np
 import matplotlib as plt
 
-single_family_data = pd.read_csv('./boston-housing-data-21-22/single_family_home.csv')
-condo_data = pd.read_csv('./boston-housing-data-21-22/median_condo_price.csv')
-black_and_latino_data = pd.read_csv('./boston-housing-data-21-22/black_and_latino_mortgage_rates.csv')
+single_family_data = read_csv('single_family_home.csv')
+condo_data = read_csv('median_condo_price.csv')
+black_and_latino_data = read_csv('black_and_latino_mortgage_rates.csv')
+
+# Print first 5 rows of values
+print(single_family_data.head())
+print(condo_data.head())
+print(black_and_latino_data.head())
+
+# Print last 5 rows of values
+print(condo_data.tail())
+print(condo_data.tail())
+print(black_and_latino_data.tail())
