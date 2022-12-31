86b919044593803c5521edf0c30f42b35aa37188
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-31 19:33:31 +0000

    added data and assigned vars

--- boston-housing-data-21-22/bmp-data.py
+++ boston-housing-data-21-22/bmp-data.py
@@ -3,3 +3,7 @@ import pandas as pd
 from pandas import read_csv
 import numpy as np
 import matplotlib as plt
+
+single_family_data = pd.read_csv('./boston-housing-data-21-22/single_family_home.csv')
+condo_data = pd.read_csv('./boston-housing-data-21-22/median_condo_price.csv')
+black_and_latino_data = pd.read_csv('./boston-housing-data-21-22/black_and_latino_mortgage_rates.csv')
